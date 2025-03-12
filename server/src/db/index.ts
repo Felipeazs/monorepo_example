0e90021170d7f9b322ffa005114016938e827ca7
felipeazs <felipeazs@gmail.com>
2025-03-12 12:09:30 -0300

    mongo fixes prod

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -16,20 +16,23 @@ const mongo = new MongoClient(env.DATABASE_URI, {
 
 export async function runDB() {
 	try {
-		await mongo.connect().then(() => console.warn("Connecting to DB..."))
+		await mongo.connect()
+
 		await mongo.db(env.NODE_ENV).command({ ping: 1 })
 		console.warn("Connected to DB")
 	} catch (err: any) {
+		await mongo.close()
 		throw new HTTPException(500, { message: err.message })
 	} finally {
-		await mongo.close()
+		await mongo
+			.db(env.NODE_ENV)
+			.createCollection("usuario", { validator: usuario })
+			.then((res) => res.createIndex({ email: 1 }, { unique: true }))
+			.then((_) => console.warn("Collections created"))
 	}
 }
 
 export const db = mongo.db(env.NODE_ENV)
-db.createCollection("usuario", { validator: usuario }).then((res) =>
-	res.createIndex({ email: 1 }, { unique: true }),
-)
 
 export const client = {
 	usuario: db.collection<Usuario>("usuario"),
