05becce8c7f656d672a3183e1801157bd831fa6f
felipeazs <felipeazs@gmail.com>
2025-03-12 13:34:17 -0300

    mongoose conexión a collection

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -7,9 +7,14 @@ export async function runDB() {
 	mongoose.set("strictQuery", true)
 
 	try {
-		await mongoose.connect(env.DATABASE_URI)
+		mongoose.connect(env.DATABASE_URI)
+
 		mongoose.connection.on("open", () => {
-			console.warn("Connected to DB ", env.NODE_ENV)
+			console.warn("Connected to DB")
+		})
+
+		mongoose.connection.once("open", () => {
+			mongoose.connection.db?.collection(env.NODE_ENV)
 		})
 	} catch (err: any) {
 		throw new HTTPException(500, { message: err.message })
