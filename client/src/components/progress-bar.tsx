3f5649026ce3eed93b0357e45c9b7a93e9effd5c
felipeazs <felipeazs@gmail.com>
2025-03-19 16:27:23 -0300

    progress bar + interval

--- client/src/components/progress-bar.tsx
+++ client/src/components/progress-bar.tsx
@@ -6,11 +6,14 @@ export function ProgressBar({ isLoading }: { isLoading: boolean }) {
 	const [progress, setProgress] = useState<number>(0)
 
 	useEffect(() => {
+		let interval: Timer
 		if (isLoading) {
-			setProgress((prevState) => prevState + 0.05)
+			interval = setInterval(() => setProgress((prevState) => prevState + 1), 20)
 		} else {
 			setProgress(100)
 		}
+
+		return () => clearInterval(interval)
 	}, [isLoading])
 
 	return <Progress max={100} className={isLoading ? "bg-blue-300" : "bg-white"} value={progress} />
