2b4c5194f29f374202876c465ad77acc356f263f
felipeazs <felipeazs@gmail.com>
2025-03-28 10:41:00 -0300

    better index layout

--- client/src/routes/_layout.index.tsx
+++ client/src/routes/_layout.index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute("/_layout/")({
 
 function Index() {
 	return (
-		<div className="flex h-[500px] flex-col items-center justify-center bg-slate-800">
+		<div className="flex h-screen flex-col items-center justify-center bg-slate-800">
 			<h1 className="bg-gradient-to-r from-yellow-800 to-yellow-100 bg-clip-text text-4xl font-bold text-transparent uppercase">
 				Monorepo Template
 			</h1>
